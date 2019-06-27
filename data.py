11b69008639a8f229176a37266c4f9ec6869e6f1
Ahsan-786 <p176142@nu.edu.pk>
2019-06-27 13:47:56 +0500

    file for testing having testing data

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'First Article',
+            'body' : 'This just a simple article.',
+            'author' : 'Brad',
+            'create-date' : '29-04-2019'
+        },
+        {
+            'id' : 2,
+            'title' : 'Second Article',
+            'body' : 'This just a simple article.',
+            'author' : 'Mack',
+            'create-date' : '29-04-2019'
+        },
+        {
+            'id' : 3,
+            'title' : 'Third Article',
+            'body' : 'This just a simple article.',
+            'author' : 'John',
+            'create-date' : '29-04-2019'
+        }
+    ]
+
+    return articles
